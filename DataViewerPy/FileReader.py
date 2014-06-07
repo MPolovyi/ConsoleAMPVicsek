79d846837fb36dc411421b064c432a181ca0e95c
Maximus <Mr_Destiny@list.ru>
2014-06-07 15:01:08 +0300

    minor changes - domain size computed

---
+++ DataViewerPy/FileReader.py
@@ -0,0 +1,25 @@
+from tkinter.filedialog import askopenfile
+import re
+import numpy as np
+
+def OpenSpdByH():
+    options = {'defaultextension': '.txt', 'filetypes': [('text files', '.txt')]}
+    file = askopenfile(mode='r', **options)
+
+    if not "Split" in file.name:
+        return
+
+    ret = {'comment': file.readline()}
+    domSize = re.search("[Dd]om.*?= (\d*\.\d*|\d*)", ret['comment']).group().split('=')[1]
+    tmpData = [re.sub('\s+', ' ', line).split(' ')[:-4] for line in file]
+    tmpData = tmpData[1:]
+
+    tmpData = zip(*tmpData)
+
+    data = [np.fromiter(item, dtype=np.float) for item in tmpData]
+    ret['data'] = data
+    ret['yMax'] = max([max(it) for it in data])
+    ret['xMax'] = float(domSize)
+    ret['xStep'] = float(domSize) / len(data[0])
+    ret['paramRange'] = len(data)
+    return ret
--- DataViewerPy/MainFile.py
+++ DataViewerPy/MainFile.py
@@ -1,1 +1,27 @@
-__author__ = 'Max'
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+from FileReader import OpenSpdByH
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(left=0.25, bottom=0.25)
+
+spdByH = OpenSpdByH()
+
+x = np.arange(0, spdByH['xMax'], spdByH['xStep'])
+mainPlot, = plt.plot(x, [0.5]*x.size, lw=2)
+plt.axis([0, spdByH['xMax'], 0, spdByH['yMax']])
+
+axcolor = 'lightgoldenrodyellow'
+axNoise = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+
+sNoise = Slider(axNoise, 'Noise', 1, spdByH['paramRange'], valinit=0)
+
+def update(val):
+    index = int(round(sNoise.val - spdByH['paramRange'], 0))
+    mainPlot.set_ydata(spdByH['data'][index])
+    fig.canvas.draw_idle()
+
+sNoise.on_changed(update)
+
+plt.show()
