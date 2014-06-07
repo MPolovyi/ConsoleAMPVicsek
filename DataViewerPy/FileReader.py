801f5053fa764cd3d33b26b898726d4dff030f48
Maximus <Mr_Destiny@list.ru>
2014-06-07 20:14:59 +0300

    series from second squashes in first's range by x

--- DataViewerPy/FileReader.py
+++ DataViewerPy/FileReader.py
@@ -2,7 +2,7 @@ from tkinter.filedialog import askopenfile
 import re
 import numpy as np
 
-def OpenSpdByH():
+def OpenSpdByH(domSize=None):
     options = {'defaultextension': '.txt', 'filetypes': [('text files', '.txt')]}
     file = askopenfile(mode='r', **options)
 
@@ -10,7 +10,9 @@ def OpenSpdByH():
         return
 
     ret = {'comment': file.readline()}
-    domSize = re.search("[Dd]om.*?= (\d*\.\d*|\d*)", ret['comment']).group().split('=')[1]
+    if domSize is None:
+        domSize = re.search("[Dd]om.*?= (\d*\.\d*|\d*)", ret['comment']).group().split('=')[1]
+
     tmpData = [re.sub('\s+', ' ', line).split(' ')[:-4] for line in file]
     tmpData = tmpData[1:]
 
@@ -22,4 +24,5 @@ def OpenSpdByH():
     ret['xMax'] = float(domSize)
     ret['xStep'] = float(domSize) / len(data[0])
     ret['paramRange'] = len(data)
+
     return ret
--- DataViewerPy/MainFile.py
+++ DataViewerPy/MainFile.py
@@ -10,7 +10,7 @@ spdByH = [OpenSpdByH()]
 
 x = np.arange(0, spdByH[0]['xMax'], spdByH[0]['xStep'])
 
-mainPlot = plt.plot(x, [0.5]*x.size, lw=2)
+mainPlot = plt.plot(x, spdByH[0]['data'][0], lw=2, label=spdByH[0]['comment'])
 plt.axis([0, spdByH[0]['xMax'], 0, spdByH[0]['yMax']])
 mainAxes = plt.gca()
 
@@ -18,23 +18,33 @@ axcolor = 'lightgoldenrodyellow'
 axNoise = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
 sNoise = Slider(axNoise, 'Noise', 1, spdByH[0]['paramRange'], valinit=0)
 
+
 def update(val):
     index = int(round(sNoise.val - spdByH[0]['paramRange'], 0))
     for i in range(0, mainPlot.__len__()):
-        mainPlot[i].set_ydata(spdByH[i]['data'][index])
+        varIndex = index
+        while varIndex > len(spdByH[i]['data']):
+            varIndex -= len(spdByH[i]['data'])
+
+        mainPlot[i].set_ydata(spdByH[i]['data'][varIndex])
     fig.canvas.draw_idle()
+
+
 sNoise.on_changed(update)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Add Series', color=axcolor, hovercolor='0.975')
 
+
 def addButtonClick(event):
     plt.sca(mainAxes)
-    tmp = OpenSpdByH()
+    tmp = OpenSpdByH(spdByH[0]['xMax'])
+    x = np.arange(0, tmp['xMax'], tmp['xStep'])
     plt.axis([0, tmp['xMax'], 0, tmp['yMax']])
     spdByH.append(tmp)
-    mainPlot.append(plt.plot(x, tmp['data'][0], lw=2)[0])
+    mainPlot.append(plt.plot(x, tmp['data'][0], lw=2, label=tmp['comment'])[0])
+
 
 button.on_clicked(addButtonClick)
-
+plt.legend(loc='upper center', shadow=True)
 plt.show()
